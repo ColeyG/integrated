c1f3a4520d5ace6e9c41ed255473a956e7c1dadb
ColeyG <cjgeerts@gmail.com>
2019-12-16 00:46:26 -0500

    No Post in logged otu

--- resources/views/components/overlay.blade.php
+++ resources/views/components/overlay.blade.php
@@ -1,5 +1,7 @@
 <section class="overlay">
-  <div class="create-post">
-    <a href="/create"><img src="{{ asset('img/send.svg') }}" alt="New Post"></a>
-  </div>
+  @if (Auth::check())
+    <div class="create-post">
+      <a href="/create"><img src="{{ asset('img/send.svg') }}" alt="New Post"></a>
+    </div>
+  @endif
 </section>
