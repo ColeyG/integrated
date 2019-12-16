eb471c4f2f3d16d9c0adb378ea648763aad7413c
ColeyG <cjgeerts@gmail.com>
2019-12-15 23:08:01 -0500

    Create Route | Overlay Style

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -21,16 +21,16 @@
       @enderror
       <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
       <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-    @error('password')
-    <span class="invalid-feedback" role="alert">
-      <strong>{{ $message }}</strong>
-    </span>
-    @enderror
-    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">                    
-    <button type="submit" class="btn btn-primary">
-      {{ __('Register') }}
-    </button>
-  </form>
-</section>
+      @error('password')
+      <span class="invalid-feedback" role="alert">
+        <strong>{{ $message }}</strong>
+      </span>
+      @enderror
+      <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+      <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">                    
+      <button type="submit" class="btn btn-primary">
+        {{ __('Register') }}
+      </button>
+    </form>
+  </section>
 @endsection
