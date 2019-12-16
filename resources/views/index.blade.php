f5b67583c42fdc2251114c938048cbb57296d669
ColeyG <cjgeerts@gmail.com>
2019-12-15 23:39:56 -0500

    Display Posts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,14 @@
   @endcomponent
   <section class="spits-wrapper">
     <div class="spits">
-      
+      @if(App\Post::all()->isNotEmpty())
+        @foreach(App\Post::all() as $post)
+          <div class="spit">
+            <h2>{{ $post->user }}</h2>
+            <p>{{ $post->content }}</p>
+          </div>
+        @endforeach
+      @endif
     </div>
   </section>
 @endsection
