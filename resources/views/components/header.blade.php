f58eeb913e5829f9945f79a6fef470752242fc7e
ColeyG <cjgeerts@gmail.com>
2019-12-15 20:36:09 -0500

    Main Page

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,8 @@
+<header class="primary-header">
+  <h1 class="title">Spitter</h1>
+  <ul>
+    <li><a href="#">Help</a></li>
+    <li><a href="#">Log In</a></li>
+    <li class="sign-up"><a href="#">Sign Up</a></li>
+  </ul>
+</header>
